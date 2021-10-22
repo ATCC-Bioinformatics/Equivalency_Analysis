b461efc6fb429750374ab235bb7698f3c2178c13
David Yarmosh <d-yarmosh@users.noreply.github.com>
2021-10-22 10:39:48 -0400

    Update parse_metadata.py

--- Database_survey/parse_metadata.py
+++ Database_survey/parse_metadata.py
@@ -1,30 +1,38 @@
 import pickle
 
-# NCTC 2 ATCC
+# parse atcc2nctc.csv and create conversion dictionary
 nctc2atcc={}
 for line in open('atcc2nctc.csv','r'):
     line = line.replace('\n','').split(',')
     nctc = line[0].replace('NCTC ','')
     atcc = line[-1].replace('ATCC ','')
     nctc2atcc[nctc]= atcc
-# print(nctc2atcc)
 
+# unpickle data from gather_metadata.py
 with open('assembly_metadata.pkl','rb') as f:
     data = pickle.load(f)
 
+# collect complete set of metadata fields from metadata dictionary
 keys_set = set()
 for k in data.keys():
     for sub_key in data[k].keys():
         keys_set.add(sub_key)
-# print(keys_set)
-
+# open file for writing out metadata
 with open('atcc_nctc_gcf-level-metadata.txt','w') as f:
+    # header contains each metadata field
     f.write('\t'.join(list(keys_set)))
     f.write('\n')
+    # add each metadata field value for each assembly to the line variable
+    # if assembly doesn't have that field, then add "n/a"
     for assembly in data.keys():
+        # line will hold the tab delimited metadata fields
         line = ''
+        # keep is for each assembly, unless it has an nctc product id with no atcc id equivalent
         keep = True
+        # go through each metadata field in the entire set of available metadata fields collected 
+        # in lines 16 through 20
         for key in keys_set:
+            # if the field is Product ID, convert nctc id to atcc id
             if key == 'Product ID':
                 if 'atcc' in data[assembly][key]:
                     line += data[assembly][key] + '\t'
@@ -34,14 +42,18 @@ with open('atcc_nctc_gcf-level-metadata.txt','w') as f:
                     if nctc in nctc2atcc.keys():
                         atcc_id = nctc2atcc[nctc]
                         line += atcc_id +' (' + prev_id + ')' + '\t'
+                    # keep is only false if there is no nctc to atcc conversion (see comment on line 30)
                     else:
                         keep = False
                 else:
                     print('problem')
+            # if the assembly_report contained the metadata field, then add the value to the line
             elif key in data[assembly].keys():
                 line += data[assembly][key] + '\t'
+            # if the assembly_report did not contain the metadata field, then add "n/a" to the line
             else:
-                line += 'n\\a\t'
+                line += "n/a\t"
+        # print line
         if keep == True:
             f.write(line)
             f.write('\n')
